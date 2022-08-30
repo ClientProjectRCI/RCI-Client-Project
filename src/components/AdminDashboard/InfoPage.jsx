ad36f0e56690b4e3b1f32301851c479c93e54df9
Avery Yang <averyyang2674@gmail.com>
2022-08-30 13:58:35 -0500

    add comments to NOT touch in admin dashboard/providerSearch; rename InfoPage to AdminDashboard; add Nav

--- src/components/AdminDashboard/InfoPage.jsx
+++ src/components/AdminDashboard/InfoPage.jsx
@@ -5,12 +5,14 @@ import React from 'react';
 // It doesn't dispatch any redux actions or display any part of redux state
 // or even care what the redux state is
 
-function InfoPage() {
-  return (
-    <div className="container">
-      <p>Info Page</p>
-    </div>
-  );
+//AVERY: DON'T TOUCH FILE
+
+function AdminDashboard() {
+    return (
+        <div className="container">
+            <p>Info Page</p>
+        </div>
+    );
 }
 
-export default InfoPage;
+export default AdminDashboard;
