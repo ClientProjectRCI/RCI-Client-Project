f11626c6747e7057ab10c62d5bdaabc87a75d3fe
Nate Notermann <nate.notermann@gmail.com>
2022-09-01 16:23:36 -0500

    added occupation table and it's junction table

--- database.sql
+++ database.sql
@@ -62,7 +62,13 @@ CREATE TABLE "service_type" (
 	"id" serial PRIMARY KEY NOT NULL,
 	"service" varchar(255) 
 );
+---- occupations TABLE ----
+CREATE TABLE "occupations" (
+	"id" serial PRIMARY KEY NOT NULL,
+	"occupation" varchar(255) 
+);
 
+-- JUNCTION TABLES --
 
 ---- specializations JUNCTION TABLE ----
 CREATE TABLE "provider_specializations" (
@@ -75,7 +81,7 @@ CREATE TABLE "provider_specializations" (
 CREATE TABLE "provider_insurance_plan" (
 	"id" serial PRIMARY KEY NOT NULL,
 	"provider_id" int REFERENCES "provider"("id") NOT NULL,
-	"insruance_plan_id" int REFERENCES "insurance_plan"("id") NOT NULL
+	"insurance_plan_id" int REFERENCES "insurance_plan"("id") NOT NULL
 );
 
 ---- service_type JUNCTION TABLE ----
@@ -85,21 +91,12 @@ CREATE TABLE "provider_service_type" (
 	"service_type_id" int REFERENCES "service_type"("id") NOT NULL
 );
 
-
--- DUMMY DATA -- 
---  USER Dummy Data --
-INSERT INTO "user" ("id", "username", "password", "access_level") VALUES 
-(1, 'admin', 'admin', 1),
-(2, 'group', 'group', 2),
-(3, 'provider', 'provider', 2);
-
---  GROUP Dummy Data --
-INSERT INTO "group" ( "id", "user_id", "name", "bio", "website", "email", "phone", "street", "city", "state", "zipcode") VALUES 
-(1, 2, 'group1', 'bio', 'website', 'email', 'phone', 'street', 'city', 'state', 'zipcode');
-
---  PROVIDER Dummy Data --
-INSERT INTO "provider" ("id", "user_id", "name", "bio", "picture", "email", "insurance_id", "occupation_id", "specialization_id", "service_id", "availability", "group_id") VALUES
-(1, 3, 'provider1', 'bio', 'pic', 'email', 1, 1, 1, 1, 'availability', 1);
+---- provider_occupation JUNCTION TABLE ----
+CREATE TABLE "provider_occupation" (
+	"id" serial PRIMARY KEY NOT NULL,
+	"provider_id" int REFERENCES "provider"("id") NOT NULL,
+	"occupation_id" int REFERENCES "specializations"("id") NOT NULL
+);
 
 
 -- ACTUAL DATA --
@@ -141,16 +138,68 @@ INSERT INTO "insurance_plan" ("insurance") VALUES
 ('Humana'),
 ('Cigna'),
 ('Silverscript'),
-('Medicare Insurance Providers in Minnesota')
-;
+('Medicare Insurance Providers in Minnesota');
+
 ---- INSERT service_type TABLE ----
 INSERT INTO "service_type" ("service") VALUES
 ('Online'),
 ('In-Person'),
 ('Over the Phone'),
 ('Inpatient'),
-('Outpatient')
-;
+('Outpatient');
+
+---- INSERT occupation TABLE ----
+INSERT INTO "occupations" ("occupation") VALUES
+('Psychologist'),
+('Counselor'),
+('Certified Alcohol and Drug Abuse Counselor'),
+('Clinician'),
+('Clinical Social Worker'),
+('Psychiatrist'),
+('Mental Health Nurse Practitioner'),
+('Family Nurse Practitioner'),
+('Peer Specialist');
+
+-- DUMMY DATA -- 
+--  USER Dummy Data --
+INSERT INTO "user" ("id", "username", "password", "access_level") VALUES 
+(1, 'admin', 'admin', 1),
+(2, 'group', 'group', 2),
+(3, 'provider', 'provider', 2);
+
+--  GROUP Dummy Data --
+INSERT INTO "group" ( "id", "user_id", "name", "bio", "website", "email", "phone", "street", "city", "state", "zipcode") VALUES 
+(1, 2, 'group1', 'bio', 'website', 'email', 'phone', 'street', 'city', 'state', 'zipcode');
+
+--  PROVIDER Dummy Data --
+INSERT INTO "provider" ("id", "user_id", "name", "bio", "picture", "email", "insurance_id", "occupation_id", "specialization_id", "service_id", "availability", "group_id") VALUES
+(1, 3, 'provider1', 'bio', 'pic', 'email', 1, 1, 1, 1, 'availability', 1);
+
+-- provider_specializations JUNCTION TABLE DUMMY DATA --  
+INSERT INTO "provider_specializations" ( "provider_id", "specializations_id") VALUES 
+( 1, 1), -- LGBTQ
+( 1, 6), -- ADHD
+( 1, 11); -- COVID
+
+
+-- provider_specializations JUNCTION TABLE DUMMY DATA --  
+INSERT INTO "provider_insurance_plan" ( "provider_id", "insurance_plan_id") VALUES 
+( 1, 2), -- HealthPartners
+( 1, 3), -- Medica of Minnesota
+( 1, 10); -- CIGNA
+
+
+-- provider_specializations JUNCTION TABLE DUMMY DATA --  
+INSERT INTO "provider_service_type" ( "provider_id", "service_type_id") VALUES 
+( 1, 1), -- Online
+( 1, 2), -- InPerson
+( 1, 4); -- Inpatient
+
+
+INSERT INTO "provider_occupation" ( "provider_id", "occupation_id") VALUES 
+(1, 1),
+(1, 2),
+(1, 2);
 
 ---- STOP -- COPY -- & -- PASTE -- HERE ----
 
@@ -175,6 +224,7 @@ ORDER BY "service";
 SELECT * FROM "user" WHERE "user"."id" = 1; -- GET ONE USER BY "id" example: 1
 SELECT * FROM "group" WHERE "group"."id" = 1; -- GET ONE GROUP BY "id" example: 1
 SELECT * FROM "provider" WHERE "provider"."id" = 1; -- GET ONE PROVIDER BY "id" example: 1
+SELECT * FROM "provider" WHERE "provider"."id" = 1; -- GET ONE PROVIDER BY "id" example: 1
 
 --GET ONE GROUP BY USER.ID, example: 2
 SELECT * FROM "group" 
@@ -184,17 +234,21 @@ JOIN "user" ON "group"."user_id" = "user"."id" WHERE "user"."id" = 2;
 SELECT * FROM "provider" 
 JOIN "user" ON "provider"."user_id" = "user"."id" WHERE "user"."id" = 3;
 
+SELECT * FROM "provider_specializations" WHERE "provider_id" = 1; -- GET ONE PROVIDERS SPECIALIZATIONS BY "provider_id" example: 1
+SELECT * FROM "provider_insurance_plan" WHERE "provider_id" = 1; -- GET ONE PROVIDERS INSURANCE_PLAN BY "provider_id" example: 1
+SELECT * FROM "provider_service_type" WHERE "provider_id" = 1; -- GET ONE PROVIDERS SPECIALIZATIONS BY "provider_id" example: 1
+
 
 -----! DELETE/DROP TABLES !-----
 DROP TABLE "provider_specializations";
 DROP TABLE "provider_insurance_plan";
 DROP TABLE "provider_service_type";
+DROP TABLE "provider_occupation";
 DROP TABLE "specializations";
 DROP TABLE "insurance_plan";
 DROP TABLE "service_type";
+DROP TABLE "occupations";
 DROP TABLE "provider";
 DROP TABLE "group";
 DROP TABLE "user"; 
 -----! DELETE/DROP TABLES !-----
-
-
