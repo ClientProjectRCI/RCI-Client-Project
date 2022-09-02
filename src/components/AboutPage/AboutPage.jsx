b57d6a5a92bb33c827c0fdadb2ec9242214aa051
Avery Yang <averyyang2674@gmail.com>
2022-09-02 16:02:13 -0500

    build out sections for about us

--- src/components/AboutPage/AboutPage.jsx
+++ src/components/AboutPage/AboutPage.jsx
@@ -5,14 +5,32 @@ import React from 'react';
 // It doesn't dispatch any redux actions or display any part of redux state
 // or even care what the redux state is'
 
-function AboutPage() {
-  return (
-    <div className="container">
-      <div>
-        <p>This about page is for anyone to read!</p>
-      </div>
-    </div>
-  );
+export default function AboutPage() {
+    return (
+        <div className="container">
+            <section>
+                <h1>I. About RCI</h1>
+                We are a nonprofit dedicated to improving the Rochester area and
+                beyond through education and advocacy. We work to provide a
+                platform for youth empowerment and their education and to serve
+                underprivileged demographics.{' '}
+            </section>
+            <section>
+                <h1>II. Project Background</h1>
+                In 2021, we conducted a survey of over 1,000 people in Rochester
+                about their mental health and mental health resources. We
+                discovered that many people know that resources in the area are
+                available, but have a difficult time finding or accessing them.
+                We decided to develop an active database of local mental health
+                resources.
+            </section>
+            <section>
+                <h1>III. Mission</h1>
+                The purpose of this project is to provide easier access to
+                mental health resources. Additionally, this database is for the
+                high school counselors of Century, Mayo, and John Marshall High
+                Schools to use to direct students towards available aid.
+            </section>
+        </div>
+    );
 }
-
-export default AboutPage;
