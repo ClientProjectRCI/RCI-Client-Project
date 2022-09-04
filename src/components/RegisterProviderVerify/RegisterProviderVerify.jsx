463f82ad368b8ebc11aacb0f1bd41d051a00ab3e
Todd McCoy <tcmccoy@gmail.com>
2022-09-04 11:05:12 -0500

    verify page

--- src/components/RegisterProviderVerify/RegisterProviderVerify.jsx
+++ src/components/RegisterProviderVerify/RegisterProviderVerify.jsx
@@ -12,29 +12,29 @@ export default function RegisterProviderForm() {
     const [providerBio, setProviderBio]= useState('');
     const [providerPhone, setProviderPhone]= useState('');
     const [providerEmail, setProviderEmail]= useState('');
-    const [providerWebsite, setProviderWebsite]= useState('');
+    // const [providerWebsite, setProviderWebsite]= useState('');
     const [providerInsurance, setProviderInsurance]= useState('');
     const [providerOccupation, setProviderOccupation]= useState('');
     const [providerSpecialization, setProviderSpecialization]= useState('');
     const [providerService, setProviderService]= useState('');
     const [providerAvailability, setProviderAvailability]= useState('');
 
-    const addFile = (event) => setNewPath(event.target.files[0]);
+    // const addFile = (event) => setNewPath(event.target.files[0]);
 
-    const sendImage = (event) => {
-		const data = new FormData();
+    // const sendImage = (event) => {
+	// 	const data = new FormData();
 		
-		data.append('image', newPath);
+	// 	data.append('image', newPath);
 
-		axios
-			.post('api/provider/image', data)
-			.then((response) => {
-				console.log(response);
-			})
-			.catch((error) => {
-				alert('Error with post', error);
-			});
-	};
+	// 	axios
+	// 		.post('api/provider/image', data)
+	// 		.then((response) => {
+	// 			console.log(response);
+	// 		})
+	// 		.catch((error) => {
+	// 			alert('Error with post', error);
+	// 		});
+	// };
 
     const registerProvider = () => {
             sendImage();
@@ -68,64 +68,11 @@ export default function RegisterProviderForm() {
 
     return (
         <center>
-            <form className="container" encType="multipart/form-data" onSubmit={registerProvider}>
+            <div className="container" >
                 <h2>Welcome, {user.username}!</h2>
                 <p>ProviderProfile: Your ID is: {user.id}</p>
-                <input required type="text" 
-                placeholder="Name" 
-                value={providerName}
-                onChange={(event) => setProviderName(event.target.value)}
-                ></input>
-                <input required type="text" 
-                placeholder="Bio" 
-                value= {providerBio}
-                onChange={(event) => setProviderBio(event.target.value)}
-                ></input>
-                <input required type="text" 
-                placeholder="Insurance" 
-                value={providerInsurance}
-                onChange={(event) => setProviderInsurance(event.target.value)}
-                ></input>
-                <input required type="text" 
-                placeholder="Occupation" 
-                value={providerOccupation}
-                onChange={(event) => setProviderOccupation(event.target.value)}
-                ></input>
-                <input
-                    required
-                    type="text"
-                    placeholder="Specialization(s)"
-                    value={providerSpecialization}
-                    onChange={(event) => setProviderSpecialization(event.target.value)}
-                ></input>
-                <input required type="text" 
-                placeholder="Service" 
-                value={providerService}
-                onChange={(event) => setProviderService(event.target.value)}
-                ></input>
-                <input required type="date" 
-                placeholder="Availability" 
-                value={providerAvailability}
-                onChange={(event) => setProviderAvailability(event.target.value)}
-                ></input>
-                <input required type="text" 
-                placeholder="Email" 
-                value={providerEmail}
-                onChange={(event) => setProviderEmail(event.target.value)}
-                ></input>
-                <input required type="text" 
-                placeholder="Website" 
-                value={providerWebsite}
-                onChange={(event) => setProviderWebsite(event.target.value)}
-                ></input>
-                <input required type="text" 
-                placeholder="Phone Number" 
-                value={providerPhone}
-                onChange={(event) => setProviderPhone(event.target.value)}
-                ></input>
-                <input type="file" className="file-upload" name="profile-image" onChange={addFile}></input>
-                <button onClick={registerProvider}>Submit</button>
-            </form>
+                
+            </div>
             <LogOutButton className="btn" />
         </center>
     );
