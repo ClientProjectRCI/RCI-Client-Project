84a8cc32d42e50501956e57c77f882eae9b4f197
Avery Yang <averyyang2674@gmail.com>
2022-09-09 18:09:38 -0500

    convert into mui; make password field blurred

--- src/components/RegisterPage/RegisterPage.jsx
+++ src/components/RegisterPage/RegisterPage.jsx
@@ -1,20 +1,11 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
 import RegisterForm from '../RegisterForm/RegisterForm';
-import { Typography, Box } from '@material-ui/core';
+// import { Typography, Box } from '@material-ui/core';
 
 export default function RegisterPage() {
     return (
         <center>
             <RegisterForm />
-            <Link to="/login">Already have an account? Login</Link>
-            <Box sx={{ color: 'gray', mt: 20 }}>
-                <Typography variant="p">
-                    Copyright © 2022 Rochester Community Initiative.
-                </Typography>
-                <br></br>
-                <Typography variant="p">All Rights Reserved</Typography>
-            </Box>
         </center>
     );
 }
