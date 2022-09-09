6aa1c32937ff65f4ce99696d359884e5cb96face
Avery Yang <averyyang2674@gmail.com>
2022-09-09 15:42:05 -0500

    add marginTop to FormControl; removed Box

--- src/components/RegisterForm/RegisterForm.jsx
+++ src/components/RegisterForm/RegisterForm.jsx
@@ -51,89 +51,79 @@ export default function RegisterForm() {
     }; // end provider register
 
     return (
-        <Box mt={5}>
-            <FormControl
-                style={{
-                    width: '25rem',
-                    paddingBottom: '2rem',
-                    height: 'fit-content',
-                    display: 'flex',
-                    alignSelf: 'flex-end',
-                    borderRadius: 10,
-                }}
-            >
-                <Typography variant="h4" sx={{ fontWeight: 'bold' }}>
-                    Create Account
-                </Typography>
-                <Grid item margin={3}>
-                    {errors.registrationMessage && (
-                        <Typography variant="h6" className="alert" role="alert">
-                            {errors.registrationMessage}
-                        </Typography>
-                    )}
+        <FormControl
+            style={{
+                width: '25rem',
+                paddingBottom: '2rem',
+                height: 'fit-content',
+                display: 'flex',
+                alignSelf: 'flex-end',
+                borderRadius: 10,
+                marginTop: '3rem',
+            }}
+        >
+            <Typography variant="h4" sx={{ fontWeight: 'bold' }}>
+                Create Account
+            </Typography>
+            <Grid item margin={3}>
+                {errors.registrationMessage && (
+                    <Typography variant="h6" className="alert" role="alert">
+                        {errors.registrationMessage}
+                    </Typography>
+                )}
+            </Grid>
+            <Grid container spacing={2} justifyContent="center">
+                <Grid item xs={12} sm={12} md={12} lg={12} xl={12}>
+                    <TextField
+                        fullWidth
+                        required
+                        variant="outlined"
+                        label="Username"
+                        value={username}
+                        onChange={(event) => setUsername(event.target.value)}
+                    />
                 </Grid>
-                <Grid container spacing={2} justifyContent="center">
-                    <Grid item xs={12} sm={12} md={12} lg={12} xl={12}>
-                        <TextField
-                            fullWidth
-                            required
-                            variant="outlined"
-                            label="Username"
-                            value={username}
-                            onChange={(event) =>
-                                setUsername(event.target.value)
-                            }
-                        />
+                <Grid item xs={12} sm={12} md={12} lg={12} xl={12}>
+                    <TextField
+                        fullWidth
+                        required
+                        variant="outlined"
+                        label="Password"
+                        value={password}
+                        onChange={(event) => setPassword(event.target.value)}
+                    />
+                </Grid>
+                <Grid container justifyContent="center" spacing={2} margin={1}>
+                    <Grid item>
+                        <Button
+                            sx={{
+                                width: 'fit-content',
+                                backgroundColor: 'var(--true-orange)',
+                            }}
+                            size="large"
+                            variant="contained"
+                            value="Provider"
+                            onClick={registerProvider}
+                        >
+                            Individual
+                        </Button>
                     </Grid>
-                    <Grid item xs={12} sm={12} md={12} lg={12} xl={12}>
-                        <TextField
-                            fullWidth
-                            required
-                            variant="outlined"
-                            label="Password"
-                            value={password}
-                            onChange={(event) =>
-                                setPassword(event.target.value)
-                            }
-                        />
-                    </Grid>
-                    <Grid
-                        container
-                        justifyContent="center"
-                        spacing={2}
-                        margin={1}
-                    >
-                        <Grid item>
-                            <Button
-                                sx={{
-                                    width: 'fit-content',
-                                    backgroundColor: 'var(--true-orange)',
-                                }}
-                                size="large"
-                                variant="contained"
-                                value="Provider"
-                                onClick={registerProvider}
-                            >
-                                Individual
-                            </Button>
-                        </Grid>
-                        <Grid item>
-                            <Button
-                                sx={{
-                                    width: 'fit-content',
-                                    backgroundColor: 'var(--cyan)',
-                                }}
-                                size="large"
-                                variant="contained"
-                                value="Provider"
-                                onClick={registerGroup}
-                            >
-                                Group
-                            </Button>
-                        </Grid>
+                    <Grid item>
+                        <Button
+                            sx={{
+                                width: 'fit-content',
+                                backgroundColor: 'var(--cyan)',
+                            }}
+                            size="large"
+                            variant="contained"
+                            value="Provider"
+                            onClick={registerGroup}
+                        >
+                            Group
+                        </Button>
                     </Grid>
                 </Grid>
-            </FormControl>
-        </Box>
+            </Grid>
+        </FormControl>
     );
 }
