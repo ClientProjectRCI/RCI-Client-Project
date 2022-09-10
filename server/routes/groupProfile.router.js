870d5f619f9e20f5424adf26a65f787b2d142889
Miski Ali <miskiali222@gmail.com>
2022-09-10 00:02:42 -0500

    made edit route

--- server/routes/groupProfile.router.js
+++ server/routes/groupProfile.router.js
@@ -22,6 +22,28 @@ router.get('/:id', (req, res) => {
     });
 });
 
+// PUT route
+router.put('/:id', (req, res) => {
+  const queryText = `UPDATE group SET name = $1, 
+  bio = $2, website = $3, 
+  email = $4, phone = $5, 
+  street = $6, city = $7, 
+  state = $8, zipcode = $9 WHERE user_id = $10;`;
+  pool.
+  query(queryText[req.body.name, req.body.bio, 
+    req.body.website, req.body.email, req.body.phone, 
+    req.body.street, req.body.city, req.body.state, 
+    req.body.zipcode, req.params.user_id])
+  .then((result)=>{
+    console.log('PUT works!!', result);
+    res.sendStatus(201)
+    })
+.catch((error)=>{
+    console.log('PUT is not working FAIL!', error);
+  })
+})
+
+
 /**
  * POST route template
  */
