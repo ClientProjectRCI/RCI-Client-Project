7ddf6c97abec58c61972486e5d179bf6fce43ea6
Avery Yang <averyyang2674@gmail.com>
2022-09-10 14:49:38 -0500

    converted provider-details buttons into mui

--- src/components/DeleteBtn/DeleteProviderBtn.jsx
+++ src/components/DeleteBtn/DeleteProviderBtn.jsx
@@ -1,20 +1,18 @@
 import React from 'react';
-import { useDispatch,} from "react-redux"; 
-import {  useSelector } from 'react-redux';
-import { useHistory } from "react-router-dom";
+import { useDispatch } from 'react-redux';
+import { useSelector } from 'react-redux';
+import { useHistory } from 'react-router-dom';
 import { Button } from '@material-ui/core';
-import Swal from 'sweetalert2'
+import Swal from 'sweetalert2';
+import DeleteForeverIcon from '@mui/icons-material/DeleteForever';
 
-
-export default function DeleteProviderBtn(){
-
+export default function DeleteProviderBtn() {
     const history = useHistory();
     const dispatch = useDispatch();
     const details = useSelector((store) => store.details);
-    console.log('what is in the>>>>>>',details);
 
     // delete function + sweetalert
-    const handleDelete =()=>{
+    const handleDelete = () => {
         Swal.fire({
             title: 'Are you sure?',
             text: "You won't be able to revert this!",
@@ -22,30 +20,30 @@ export default function DeleteProviderBtn(){
             showCancelButton: true,
             confirmButtonColor: '#3085d6',
             cancelButtonColor: '#d33',
-            confirmButtonText: 'Yes, delete it!'
+            confirmButtonText: 'Yes, delete it!',
         }).then((result) => {
-    if (result.isConfirmed) {
-        dispatch({
-            type:'DELETE_PROVIDERS',
-            payload: details.id,
-        })
-        console.log('deleteBtn is WHAT??????', details.id);
-        history.push('/providers')
-        Swal.fire(
-            'Deleted!',
-            'Your file has been deleted.',
-            'success'
-        )
-        }
-    })
-}
+            if (result.isConfirmed) {
+                dispatch({
+                    type: 'DELETE_PROVIDERS',
+                    payload: details.id,
+                });
+                console.log('deleteBtn is WHAT??????', details.id);
+                history.push('/providers');
+                Swal.fire('Deleted!', 'Your file has been deleted.', 'success');
+            }
+        });
+    };
 
-    return(
-        <Button 
-        variant="contained"
-        style={{backgroundColor: 'maroon', color: 'white'}}
-        // color="success"
-        onClick={handleDelete} >Delete</Button>
-    )
+    return (
+        <Button
+            fullWidth
+            size="medium"
+            variant="contained"
+            style={{ backgroundColor: 'maroon', color: 'white' }}
+            // color="success"
+            onClick={handleDelete}
+        >
+            <DeleteForeverIcon /> Remove This Account
+        </Button>
+    );
 }
-
