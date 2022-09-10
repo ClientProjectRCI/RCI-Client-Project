41274206be3e69c5c992aab0cade55703cdf86ab
Avery Yang <averyyang2674@gmail.com>
2022-09-09 22:07:57 -0500

    clg profileInsurances

--- src/components/ProfileInsurances/ProfileInsurances.jsx
+++ src/components/ProfileInsurances/ProfileInsurances.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { useSelector } from 'react-redux';
 export default function ProfileInsurances() {
     const profileInsurances = useSelector((store) => store.profileInsurances);
+    console.log(profileInsurances);
     return (
         <>
             {profileInsurances.map((insurance, i) => {
