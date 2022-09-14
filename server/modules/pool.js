8c5fc2d25eaf5a10fb0b3f751b24c669f02c0398
Isaac Brist <isaacbrist@gmail.com>
2022-09-14 13:45:32 -0500

    trying to fix a bug

--- server/modules/pool.js
+++ server/modules/pool.js
@@ -11,7 +11,7 @@ const url = require('url');
 let config = {};
 
 if (process.env.DATABASE_URL) {
-  // Heroku gives a url, not a connection object
+  // Heroku gives a url, not a connection object 
   // https://github.com/brianc/node-pg-pool
   const params = url.parse(process.env.DATABASE_URL);
   const auth = params.auth.split(':');
